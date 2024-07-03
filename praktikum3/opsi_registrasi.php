d7dc2644743dcf97c4c45655b369381a4fad0a49
Fatiyaaam <147456107+Fatiyaaam@users.noreply.github.com>
2024-07-03 23:32:24 +0700

    Add files via upload

---
+++ praktikum3/opsi_registrasi.php
@@ -0,0 +1,25 @@
+<?php 
+$ar_prodi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+$ar_skill = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "RWD Boostrap" => 20, 
+    "PHP" => 30,
+    "Phyton" => 30,
+    "Java" => 50,
+];
+
+$ar_domisili = [
+    "Jakarta",
+    "Bogor",
+    "Depok",
+    "Tangerang",
+    "Bekasi",
+    "Lainnya"
+];
